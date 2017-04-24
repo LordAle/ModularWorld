0ea92b21150dacf30a51c0ab2e7baa0a47e89416
LordAle <lordale29@gmail.com>
2017-04-24 15:48:39 -0400

    Adding special view table

--- starter.py
+++ starter.py
@@ -37,6 +37,8 @@ class Controller(QtWidgets.QMainWindow, Ui_MainWindow):
         self.pushButtonDeleteBuilding.clicked.connect(self.delete_building_click)
         self.pushButtonDeleteCharacter.clicked.connect(self.delete_character_click)
 
+        self.pushButtonShowFamily.clicked.connect(self.show_family_click)
+
         self.actionNewDatabase.triggered.connect(self.action_new_database)
         self.actionLoadDatabase.triggered.connect(self.action_load_database)
 
@@ -83,10 +85,12 @@ class Controller(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.table_content_model = models.SqlRelationalModel(db=self.QConnection)
         self.tableViewContent.setModel(self.table_content_model)
-        self.tableViewContent.hideColumn(0)
         self.tableViewContent.setItemDelegate(QtSql.QSqlRelationalDelegate(self.tableViewContent))
-        self.side_view_name_model = models.StringListModel()
-        self.listViewSelectedName.setModel(self.side_view_name_model)
+
+        self.table_special_model = models.SqlRelationalModel(db=self.QConnection)
+        self.tableViewSpecial.setModel(self.table_special_model)
+        self.tableViewSpecial.setItemDelegate(QtSql.QSqlRelationalDelegate(self.tableViewSpecial))
+
         self.side_view_value_model = models.StringListModel()
         self.listViewSelectedValue.setModel(self.side_view_value_model)
 
@@ -96,6 +100,8 @@ class Controller(QtWidgets.QMainWindow, Ui_MainWindow):
 
 
 # View query methods ---------------------------------------------------------------Begin
+
+    #Selected object ID
 
     def get_selected_city_id(self):
         city_index = self.listViewCities.currentIndex()
@@ -115,6 +121,8 @@ class Controller(QtWidgets.QMainWindow, Ui_MainWindow):
         character_id = character_record.field(1).value()
         return character_id
 
+    #Set correct list for city / building / character
+
     def query_cities_name(self):
         self.cities_list_model.clear()
         query = QtSql.QSqlQuery("SELECT name, id FROM cities")
@@ -130,35 +138,67 @@ class Controller(QtWidgets.QMainWindow, Ui_MainWindow):
         query = QtSql.QSqlQuery("SELECT name, id FROM characters WHERE building_id = {0}".format(building_id))
         self.characters_list_model.setQuery(query)
 
-    def set_city_side_view(self, city_id):
-        self.side_view_name_model.setStringList(city.City.names_list)
+    #Side view
 
+    def set_city_side_view(self, city_id):
+        self.side_values_list = ['ID: {0}'.format(city_id)]
         selected_city = self.get_cities_from_db('id', city_id)
-        self.side_values_list = selected_city[0].values_list
+        selected_city = selected_city[0]
+
+        self.side_values_list.append('{0} of {1}'.format(selected_city.kind, selected_city.name))
+        self.side_values_list.append('Population of {0}'.format(selected_city.population))
+        self.side_values_list.append('Most inhabitants are {0}'.format(selected_city.main_race))
+        self.side_values_list.append('Geographical features are :')
+        if selected_city.plains:
+            self.side_values_list.append('  Plains')
+        if selected_city.forests:
+            self.side_values_list.append('  Forests')
+        if selected_city.river:
+            self.side_values_list.append('  River')
+        if selected_city.sea:
+            self.side_values_list.append('  Sea')
+        if selected_city.mountains:
+            self.side_values_list.append('  Mountains')
+        if selected_city.mines:
+            self.side_values_list.append('  Mines')
+
         for x in range(len(self.side_values_list)):
             if type(self.side_values_list[x]) is not str:
                 self.side_values_list[x] = str(self.side_values_list[x])
         self.side_view_value_model.setStringList(self.side_values_list)
 
     def set_building_side_view(self, building_id):
-        self.side_view_name_model.setStringList(building.Building.names_list)
-
+        self.side_values_list = ['ID: {0}'.format(building_id)]
         selected_building = self.get_buildings_from_db('id', building_id)
-        self.side_values_list = selected_building[0].values_list
+        selected_building = selected_building[0]
+
+        self.side_values_list.append(selected_building.name)
+        self.side_values_list.append('{0} {1}'.format(selected_building.subkind, selected_building.kind))
+
         for x in range(len(self.side_values_list)):
             if type(self.side_values_list[x]) is not str:
                 self.side_values_list[x] = str(self.side_values_list[x])
         self.side_view_value_model.setStringList(self.side_values_list)
 
     def set_character_side_view(self, character_id):
-        self.side_view_name_model.setStringList(character.Character.names_list)
-
+        self.side_values_list = ['ID: {0}'.format(character_id)]
         selected_character = self.get_characters_from_db('id', character_id)
-        self.side_values_list = selected_character[0].values_list
+        selected_character = selected_character[0]
+
+        self.side_values_list.append('{0} {1}'.format(selected_character.name, selected_character.fname))
+        self.side_values_list.append('{0} {1}'.format(selected_character.gender, selected_character.race))
+        self.side_values_list.append('Age: {0}'.format(selected_character.age))
+        self.side_values_list.append('Profession: {0}'.format(selected_character.profession))
+        self.side_values_list.append('Wealth: {0}'.format(selected_character.wealth))
+        self.side_values_list.append('Role: {0}'.format(selected_character.role))
+        self.side_values_list.append('Level {0} {1}'.format(selected_character.level, selected_character.classe))
+
         for x in range(len(self.side_values_list)):
             if type(self.side_values_list[x]) is not str:
                 self.side_values_list[x] = str(self.side_values_list[x])
         self.side_view_value_model.setStringList(self.side_values_list)
+
+    # Set main table model
 
     def set_building_table(self, city_id):
         self.table_content_model.setEditStrategy(models.SqlTableModel.OnFieldChange)
@@ -236,6 +276,51 @@ class Controller(QtWidgets.QMainWindow, Ui_MainWindow):
             self.table_content_model.setHeaderData(13, QtCore.Qt.Horizontal, 'Building')
             self.table_content_model.setHeaderData(14, QtCore.Qt.Horizontal, 'Visiting')
             self.table_content_model.setHeaderData(15, QtCore.Qt.Horizontal, 'Note')
+
+    #Set special table model
+
+    def show_selected_family(self):
+        selected_character = self.get_characters_from_db('id', self.get_selected_character_id())
+        selected_character = selected_character[0]
+        selected_id = selected_character.id
+        selected_parent_id = selected_character.parent_id
+        if type(selected_parent_id) is not int:
+            selected_parent_id = 0
+
+        self.table_special_model.setEditStrategy(models.SqlTableModel.OnFieldChange)
+        self.table_special_model.setTable('characters')
+        self.set_special_table_header()
+
+        #self.table_special_model.setFilter('parent_id = {0} OR parent_id = {1}'.format(selected_id,
+        # selected_parent_id))
+        self.table_special_model.setFilter('id=1')  # Parent
+        print(self.table_special_model.selectStatement())
+        #self.table_special_model.setFilter('id = {0}'.format(selected_character.id))  # Itself / Not needed
+        #self.table_special_model.setFilter('id = {0} OR parent_id = {1} OR id = {2} OR parent_id = {3}'.format(
+            #selected_character.id, selected_character.id, selected_character.parent_id, selected_character.parent_id))
+        self.table_special_model.setRelation(12, QtSql.QSqlRelation('cities', 'id', 'name'))
+        self.table_special_model.setRelation(13, QtSql.QSqlRelation('buildings', 'id', 'name'))
+        self.table_special_model.select()
+
+    def set_special_table_header(self):
+        self.table_special_model.setHeaderData(0, QtCore.Qt.Horizontal, 'ID')
+        self.table_special_model.setHeaderData(1, QtCore.Qt.Horizontal, 'Name')
+        self.table_special_model.setHeaderData(2, QtCore.Qt.Horizontal, 'Family')
+        self.table_special_model.setHeaderData(3, QtCore.Qt.Horizontal, 'Race')
+        self.table_special_model.setHeaderData(4, QtCore.Qt.Horizontal, 'Gender')
+        self.table_special_model.setHeaderData(5, QtCore.Qt.Horizontal, 'Age')
+        self.table_special_model.setHeaderData(6, QtCore.Qt.Horizontal, 'Role')
+        self.table_special_model.setHeaderData(7, QtCore.Qt.Horizontal, 'Profession')
+        self.table_special_model.setHeaderData(8, QtCore.Qt.Horizontal, 'Wealth')
+        self.table_special_model.setHeaderData(9, QtCore.Qt.Horizontal, 'Class')
+        self.table_special_model.setHeaderData(10, QtCore.Qt.Horizontal, 'Level')
+        self.table_special_model.setHeaderData(11, QtCore.Qt.Horizontal, 'Parent')
+        self.table_special_model.setHeaderData(12, QtCore.Qt.Horizontal, 'City')
+        self.table_special_model.setHeaderData(13, QtCore.Qt.Horizontal, 'Building')
+        self.table_special_model.setHeaderData(14, QtCore.Qt.Horizontal, 'Visiting')
+        self.table_special_model.setHeaderData(15, QtCore.Qt.Horizontal, 'Note')
+
+    #Qury items from DB
 
     def get_cities_from_db(self, filter_by, filter_value):
         connector = db_connector.City_Connector(self.loader)
@@ -489,6 +574,9 @@ class Controller(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def delete_character_click(self):
         self.open_delete_character_dialog()
+
+    def show_family_click(self):
+        self.show_selected_family()
 
 # View Actions methods -------------------------------------------------------------End
 
